1a733c1115a2235c4b9a9f8b63ac457ab1747cf2
Thalisson <thalissonsousa33@gmail.com>
2025-05-18 00:39:00 -0300

    feature: create card component

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  imports: [],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() icon: string = '';
+
+}
