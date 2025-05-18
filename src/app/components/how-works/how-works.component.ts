2573205f1f88edeee9a24b9c40fcb2d6a4af097d
Thalisson <thalissonsousa33@gmail.com>
2025-05-18 00:38:40 -0300

    feature: create component how works

---
+++ src/app/components/how-works/how-works.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CardComponent } from "../card/card.component";
+
+
+@Component({
+  selector: 'app-how-works',
+  imports: [CardComponent],
+  templateUrl: './how-works.component.html',
+  styleUrl: './how-works.component.scss'
+})
+export class HowWorksComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from '../../components/banner/banner.component';
+import { HowWorksComponent } from "../../components/how-works/how-works.component";
 
 @Component({
   selector: 'app-home',
-  imports: [BannerComponent],
+  imports: [BannerComponent, HowWorksComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
