9dd7686ae79df7da511b0597edd942254a4774e5
Thalisson <thalissonsousa33@gmail.com>
2025-05-17 23:40:45 -0300

    feature: home page

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BannerComponent } from '../../components/banner/banner.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [BannerComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
