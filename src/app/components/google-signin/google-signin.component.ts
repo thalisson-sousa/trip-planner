20b835aa050aab0f67c26a5c69914fc1f67a1429
Thalisson <thalissonsousa33@gmail.com>
2025-06-29 01:36:42 -0300

    feat: implements social login

---
+++ src/app/components/google-signin/google-signin.component.ts
@@ -0,0 +1,35 @@
+import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
+
+@Component({
+  selector: 'app-google-signin',
+  imports: [],
+  templateUrl: './google-signin.component.html',
+  styleUrl: './google-signin.component.scss'
+})
+export class GoogleSigninComponent {
+
+  constructor(private service: AuthenticationService, private Router: Router) { }
+
+  signInWithGoogle() {
+    this.service.loginWithGoogle().then(() => {
+
+      this.service.signOrSignUp().then(res => {
+
+         // Armazena o token do usuário no localStorage
+        localStorage.setItem('token', (res?.token));
+        // Armazena o ID do usuário no localStorage
+        localStorage.setItem('userId', (res?.id));
+
+        // Redireciona para a página inicial após o login
+         this.Router.navigate(['/']);
+      }).catch(error => {
+        console.error('Erro ao verificar ou criar usuário:', error);
+      });
+    }).catch(error => {
+      console.error('Erro ao fazer login com o Google:', error);
+    });
+}
+
+}
