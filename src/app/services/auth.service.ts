387da65678bf8c0b45f26ab0a3f0a6f3e523b8f4
Thalisson <thalissonsousa33@gmail.com>
2025-06-29 22:08:27 -0300

    feat: validate token to access the routes

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { catchError, map, Observable, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  private readonly apiUrl = `${environment.API}/api/auth`;
+  private isvalid: Boolean = false;
+
+  constructor(private http: HttpClient) {}
+
   getToken(): string | null {
     return typeof window !== 'undefined' ? localStorage.getItem('token') : null;
+  }
+
+  validateToken(): Observable<boolean> {
+    return this.http
+      .get(`${this.apiUrl}`, {
+        headers: {
+          Authorization: `Bearer ${this.getToken()}`,
+        },
+      })
+      .pipe(
+        map(() => {
+          this.isvalid = true;
+          return true;
+        }),
+        catchError((error) => {
+          console.error('Token validation failed:', error);
+          localStorage.removeItem('token');
+          localStorage.removeItem('userId');
+          this.isvalid = false;
+          return of(false);
+        })
+      );
   }
 }
--- src/app/utils/authGuard.ts
+++ src/app/utils/authGuard.ts
@@ -1,17 +1,18 @@
 import { inject } from "@angular/core";
 import { Router } from "@angular/router";
 import { AuthService } from "../services/auth.service";
+import { tap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 export const authGuard = () => {
   const router = inject(Router);
   const authService = inject(AuthService);
 
-  const token = authService.getToken();
-
-  if (token) {
-    return true;
-  } else {
-    router.navigate(['/login']);
-    return false;
-  }
+  return authService.validateToken().pipe(
+    tap((isValid) => {
+      if (!isValid) {
+        router.navigate(['/login']);
+      }
+    })
+  );
 };
